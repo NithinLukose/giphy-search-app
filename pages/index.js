f9cf5078c49c7e6ae2e95488d7b17837c29de3a5
NithinLukose <nithinclukose12@gmail.com>
2022-12-27 09:57:50 +0530

    static render with data fetching using getStaticProps
    static render with data fetching using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home(initialProps) {
+  useEffect(() => console.log(initialProps));
   return (
     <>
       <Head>
@@ -13,4 +15,13 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  let catGiphs = await fetch(
+    "https://api.giphy.com/v1/gifs/search?q=cats&api_key=qOs5aDoMpWCw3TETNjHr9eQlw8WZ0ssa&limit=10"
+  ).then((response) => response.json());
+  return {
+    props: { catGiphs },
+  };
 }
